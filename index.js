25e716e1b825d68c75b65d009b28680867b96bb8
Diyorbek <77392009+Diyorbek0404@users.noreply.github.com>
2022-03-04 15:04:34 +0500

    update index.js

--- index.js
+++ index.js
@@ -8,32 +8,33 @@ const userRouter = require("./routes/user")
 const xodimRouter = require("./routes/xodim")
 const xodimruRouter = require("./routes/xodimru")
 const messageRoute = require("./routes/message")
+const reklamaRouter = require("./routes/reklama")
 const multer = require("multer")
 const app = express();
 const cors = require("cors");
 app.use(cors())
 app.use(express.json())
 
-app.use("/images", express.static(path.join(__dirname, "/images")))
+// app.use("/images", express.static(path.join(__dirname, "/images")))
 
-mongoose.connect("mongodb+srv://kuldashev:mO5JzQd3x8annG8z@cluster0.r6vwn.mongodb.net/klinika?retryWrites=true&w=majority").then(console.log("connected to mongoDB"))
+mongoose.connect("mongodb+srv://kuldashev:mO5JzQd3x8annG8z@cluster0.s70qi.mongodb.net/klinika?retryWrites=true&w=majority").then(console.log("connected to mongoDB"))
     .catch(error => {
         console.log(error)
     })
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "images")
-    },
-    filename: (req, file, cb) => {
-        cb(null, req.body.name);
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, "images")
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, req.body.name);
+//     }
+// })
 
-const upload = multer({ storage: storage })
-app.post("/api/upload", upload.single("file"), (req, res) => {
-    res.status(200).json("File has been uploaded");
-});
+// const upload = multer({ storage: storage })
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//     res.status(200).json("File has been uploaded");
+// });
 
 app.use("/api/post/", postRouter)
 app.use("/api/postru/", postruRouter)
@@ -42,8 +43,21 @@ app.use("/api/xodimru/", xodimruRouter)
 app.use("/api/user/", userRouter)
 app.use("/api/auth/", authRouter)
 app.use("/api/message/", messageRoute)
+app.use("/api/reklama/", reklamaRouter)
 
 
+// app.use(express.static(path.join(__dirname, "./client/build")));
+
+// app.get("*", function (_, res) {
+//   res.sendFile(
+//     path.join(__dirname, "./client/build/index.html"),
+//     function (err) {
+//       if (err) {
+//         res.status(500).send(err);
+//       }
+//     }
+//   );
+// });
 
 app.listen("5000", () => {
     console.log("Backend ishga tushdi")
